267f582eb84016c8149eb251373f202764d4a9e9
JasonLaboy96 <jason_laboy96@outlook.com>
2019-11-09 15:41:07 -0500

    added connection page

--- index.php
+++ index.php
@@ -1,18 +1,6 @@
 <?php
 
-    try {
-        $conn = new PDO("sqlsrv:server = tcp:foodsaver.database.windows.net,1433; Database = foodsaver", "hacknjitfood", "pizzaMaster@");
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch (PDOException $e) {
-        print("Error connecting to SQL Server.");
-        die(print_r($e));
-    }
-
-    // SQL Server Extension Sample Code:
-    $connectionInfo = array("UID" => "hacknjitfood", "pwd" => "pizzaMaster@", "Database" => "foodsaver", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-    $serverName = "tcp:foodsaver.database.windows.net,1433";
-    $conn = sqlsrv_connect($serverName, $connectionInfo);
+    inclue("sqlConn.php");
 
     $s = "SELECT * FROM [dbo].[users]";
 
@@ -26,6 +14,5 @@
 
 
 
-    echo "Hello world!!!!!!!<br>";
 
 ?>
