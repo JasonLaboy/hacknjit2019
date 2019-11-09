1299826bcf6cb412d683a10d3c8184ad52efff2a
PKRoss <patrickr2311@gmail.com>
2019-11-09 18:29:09 -0500

    testing sms

--- send_sms.php
+++ send_sms.php
@@ -1,20 +1,3 @@
 <?php
-// Include the bundled autoload from the Twilio PHP Helper Library
-require __DIR__ . 'twilio-php-master/twilio-php-master/Twilio/autoload.php';
-use Twilio\Rest\Client;
-// Your Account SID and Auth Token from twilio.com/console
-$account_sid = 'AC23010d085a4753b32117856483aef181';
-$auth_token = 'fb7cc58969e4d2fbc71a132ba89b7b27';
-// In production, these should be environment variables. E.g.:
-// $auth_token = $_ENV["TWILIO_ACCOUNT_SID"]
-// A Twilio number you own with SMS capabilities
-$twilio_number = "+12055399401";
-$client = new Client($account_sid, $auth_token);
-$client->messages->create(
-    // Where to send a text message (your cell phone?)
-    '+18624529510',
-    array(
-        'from' => $twilio_number,
-        'body' => 'I sent this message in under 10 minutes!'
-    )
-);
+    echo getcwd(); 
+?> 
