3bbdc9fab3a6eee6cfd789f60588a70be7d87390
PKRoss <patrickr2311@gmail.com>
2019-11-09 18:49:46 -0500

    testing sms

--- send_sms.php
+++ send_sms.php
@@ -1,5 +1,5 @@
 <?php 
-    $path = __DIR__.'\twilio-php-master\twilio-php-master\src\autoload.php';
+    $path = __DIR__.'\twilio-php-master\twilio-php-master\src';
     if(file_exists($path)){
         echo "okay"; 
     }
