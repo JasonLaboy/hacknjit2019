c85c612c01be9aab8fb4b6284151fbe6df79e40f
JasonLaboy96 <jason_laboy96@outlook.com>
2019-11-09 18:23:48 -0500

    test

--- functions.php
+++ functions.php
@@ -1,5 +1,17 @@
 <?php
+    try {
+        $conn = new PDO("sqlsrv:server = tcp:foodsaver.database.windows.net,1433; Database = foodsaver", "hacknjitfood", "pizzaMaster@");
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $e) {
+        print("Error connecting to SQL Server.");
+        die(print_r($e));
+    }
 
+    // SQL Server Extension Sample Code:
+    $connectionInfo = array("UID" => "hacknjitfood", "pwd" => "pizzaMaster@", "Database" => "foodsaver", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+    $serverName = "tcp:foodsaver.database.windows.net,1433";
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
 
     function authUser($email, $password){
         global $conn;
