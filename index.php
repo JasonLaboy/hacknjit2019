23208523467e88ab65e15dfc95bf5631ca212e54
JasonLaboy96 <jason_laboy96@outlook.com>
2019-11-09 13:04:09 -0500

    added sql connection

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
+
+    try {
+        $conn = new PDO("sqlsrv:server = tcp:foodsaver.database.windows.net,1433; Database = foodsaver", "hacknjitfood", "pizzaMaster@");
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $e) {
+        print("Error connecting to SQL Server.");
+        die(print_r($e));
+    }
+
+    // SQL Server Extension Sample Code:
+    $connectionInfo = array("UID" => "hacknjitfood", "pwd" => "{your_password_here}", "Database" => "foodsaver", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+    $serverName = "tcp:foodsaver.database.windows.net,1433";
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+
     echo "Hello world!!!!!!!";
 
 ?>
