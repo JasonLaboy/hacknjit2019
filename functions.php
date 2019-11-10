3a2a1a4bd9365df98af8ce450a9b7f2b2397c593
PKRoss <patrickr2311@gmail.com>
2019-11-09 21:53:42 -0500

    Printing..

--- functions.php
+++ functions.php
@@ -1,25 +1,29 @@
 <?php
-    
-
     function authUser($email, $password, &$id){
         global $conn;
-        
         $s = "SELECT * FROM [dbo].[users] WHERE [email] = '$email' and [password] ='$password'";
         
-
         ($t = sqlsrv_query( $conn ,$s)) or die (sqlsrv_errors($conn ));
         $r = sqlsrv_fetch_array($t);
         $id = $r['id'];
 
         if (sqlsrv_has_rows ( $t )){
-            
-            
             return true;
         }
         return false;
         
-        
-
-
+    }
+    //functions gets user ID
+    function getUserID($email){
+        global $conn;
+        $sql = "SELECT id FROM users WHERE email = " . $email . ";"; 
+        $results = sqlsrv_query($conn, $sql);
+        if ($results == FALSE)
+            echo (sqlsrv_errors());
+        else {
+            $row = sqlsrv_fetch_array($results, SQLSRV_FETCH_ASSOC); 
+            return $row['id']; 
+        }
+          
     }
 ?>
--- home.php
+++ home.php
@@ -17,7 +17,22 @@
         
     </head>
     <body>
-        
+        <?php 
+            global $conn;
+            //include functions file 
+            include 'functions.php'; 
+            
+            //Printing foods connecting to account 
+            $userPkey = getuserID($_POST['email']); 
+            $tsql = "SELECT foodname FROM userfood WHERE userID = " . $userPkey . ";";
+            $results = sqlsrv_query($conn, $tsql);
+            echo ("Reading data from table" . PHP_EOL);
+            if ($getResults == FALSE)
+                echo (sqlsrv_errors());
+            while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+            echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
+    }
+        ?>
         <a href="logout.php">logout</a>
         <h2>Hello <?php echo $_SESSION['userId']; ?></h2>
         
