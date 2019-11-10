eac3c7ef9de07cae06dbb1f9dabe32a9c1597b12
PKRoss <patrickr2311@gmail.com>
2019-11-09 22:56:17 -0500

    testing

--- process.php
+++ process.php
@@ -7,7 +7,7 @@
         $password = $_POST['password'];
         if (authUser($email, $password, $userId)){
             setcookie("userId", $userId, time() + 3600, "/"); 
-            echo "<script> window.location.replace("home.php"); <script>";
+            echo "<script> window.location.replace('home.php'); <script>";
         }else{
             echo "<h3 style='color:red;'>Invaild email/password</h3> ";
         }
