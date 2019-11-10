0c790a96f691031eccd2107bec236a8f785b9c04
JasonLaboy96 <jason_laboy96@outlook.com>
2019-11-09 19:25:27 -0500

    moved code

--- home.php
+++ home.php
@@ -1,22 +1,19 @@
 <!DOCTYPE HTML5>
 <html>
+    <?php
+        session_start();
+        include ("sqlConn.php");
+        include ("functions.php");
+        
+        if (!isset($_SESSION['userId'])){
+            echo "Should redirect!";
+            header("Location: http://www.example.com/blog"); 
+            die();
+        }
+
+    ?>
     <head>
-        <?php
-                session_start();
-                include ("sqlConn.php");
-                include ("functions.php");
-                
-                if (!isset($_SESSION['userId'])){
-                    echo "Should redirect!!";
-                    header("Location: http://www.example.com/blog"); 
-                    die();
-                }
-
-                
-
-
-
-            ?>
+        
     </head>
     <body>
         
