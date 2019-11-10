190cb8ebf0d15661dcc082a8adb05e7529acf3b1
PKRoss <patrickr2311@gmail.com>
2019-11-09 19:12:55 -0500

    testing sms

--- send_sms.php
+++ send_sms.php
@@ -12,10 +12,10 @@
     $client = new Client($account_sid, $auth_token);
     $client->messages->create(
         // Where to send a text message (your cell phone?)
-        '+18624529510',
+        '+19737458397',
         array(
             'from' => $twilio_number,
-            'body' => 'Hi, my name is patrick'
+            'body' => 'wassupgirl'
         )
     );
 ?> 
